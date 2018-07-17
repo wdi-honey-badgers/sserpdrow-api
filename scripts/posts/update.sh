b2a7353f0488ee5cae40467b8a7f1b4d564524d0
DanNissen <d.edwardnissen@gmail.com>
2018-07-17 13:30:04 -0400

    Add proper data attr in update script
    -successful test of update resource with curl script

--- scripts/posts/update.sh
+++ scripts/posts/update.sh
@@ -9,8 +9,10 @@ curl "${API}${URL_PATH}/${ID}" \
   --header "Content-Type: application/json" \
 --header "Authorization: Bearer ${TOKEN}" \
 --data '{
-    "post": {
-      "text": "'"${TEXT}"'"
+  "post": {
+    "title": "'"${TITLE}"'",
+    "blog": "'"${BLOG}"'",
+    "content": "'"${CONTENT}"'"
     }
   }'
 
